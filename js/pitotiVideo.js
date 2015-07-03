9ea12709ae7df210c449dc245666096fcf567e73
Tony Glover <tony.glover@nottingham.ac.uk>
2015-07-03 01:06:06 +0100

    New storyboard layout

--- js/pitotiVideo.js
+++ js/pitotiVideo.js
@@ -1,60 +1,9 @@
 /**
- * Created by DrTone on 17/05/2015.
+ * Created by DrTone on 04/12/2014.
  */
-
-
-var videoPlayer = (function() {
-    var player;
-    var numVideos = sessionStorage.getItem('numVideos');
-    var currentVideo = 0;
-    var checkInterval = 500;
-    var videoTitle;
-    var videoSource;
-
-    return {
-        init: function() {
-            for(currentVideo= 0; currentVideo<numVideos; ++currentVideo) {
-                videoTitle = sessionStorage.getItem('video'+currentVideo);
-                if(videoTitle) break;
-            }
-
-            if(videoTitle) {
-                player = document.getElementById('videoPlayer');
-                if(player) {
-                    videoSource = document.createElement("source");
-                    videoSource.setAttribute("src", videoTitle);
-                    player.appendChild(videoSource);
-                }
-            }
-            var videoTimer = setInterval(function() {
-                //console.log("Checking");
-                if(player.ended) {
-                    if(++currentVideo >= numVideos) currentVideo = 0;
-                    player.removeChild(videoSource);
-                    //Get next video
-                    videoTitle = sessionStorage.getItem('video'+currentVideo);
-                    if(videoTitle) {
-                        videoSource = document.createElement("source");
-                        videoSource.setAttribute("src", videoTitle);
-                        player.appendChild(videoSource);
-                        player.load();
-                        player.play();
-                    }
-                }
-            }, checkInterval);
-        },
-
-        play: function() {
-            player.play();
-        }
-    }
-})();
+//Visualisation framework
 
 $(document).ready(function() {
-    //Play videos
-    videoPlayer.init();
-
-
-
+    //Do any init
+    skel.init();
 });
-
